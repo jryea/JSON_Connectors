176aaa6930506498993e20175267a17c4338b109
Jon Ryea <jryea80@gmail.com>
2025-03-19 12:43:06 -0600

    WIP

--- Revit/Import/GridImportView.xaml.cs
+++ Revit/Import/GridImportView.xaml.cs
@@ -19,17 +19,23 @@ namespace Revit.Import
             _viewModel = new GridImportViewModel(uiDoc);
             DataContext = _viewModel;
 
-            // Handle window events
-            Loaded += GridImporterView_Loaded;
+            // Set owner before showing the dialog
+            var helper = new WindowInteropHelper(this);
+            helper.Owner = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+
+            // Remove Loaded event if it's defined in XAML
+            //this.Loaded -= GridImporterView_Loaded;
+
+            // Handle closing event
             Closing += GridImporterView_Closing;
         }
 
-        private void GridImporterView_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Make dialog modal to Revit window
-            var helper = new WindowInteropHelper(this);
-            helper.Owner = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
-        }
+        //private void GridImporterView_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    // Make dialog modal to Revit window
+        //    var helper = new WindowInteropHelper(this);
+        //    helper.Owner = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+        //}
 
         private void GridImporterView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -39,6 +45,20 @@ namespace Revit.Import
             {
                 TaskDialog.Show("Import Result", message);
             }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            // The import is handled by the command
+            // Just set DialogResult and close after import is done
+            this.DialogResult = true;
+            this.Close();
         }
     }
 }

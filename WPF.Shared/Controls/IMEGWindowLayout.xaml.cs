c6db8115132e5f56dce791d409d421da85fd0d42
Jon Ryea <jryea80@gmail.com>
2025-07-02 13:44:21 -0600

    WPF refactor

--- WPF.Shared/Controls/IMEGWindowLayout.xaml.cs
+++ WPF.Shared/Controls/IMEGWindowLayout.xaml.cs
@@ -1,25 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace JSON_Connectors.Controls
+namespace WPF.Shared.Controls
 {
-    /// <summary>
-    /// Interaction logic for IMEGWindowLayout.xaml
-    /// </summary>
     public partial class IMEGWindowLayout : UserControl
     {
+        public static readonly DependencyProperty WindowTitleProperty =
+            DependencyProperty.Register(nameof(WindowTitle), typeof(string),
+            typeof(IMEGWindowLayout), new PropertyMetadata("IMEG Tool"));
+
+        public static readonly DependencyProperty MainContentProperty =
+            DependencyProperty.Register(nameof(MainContent), typeof(object),
+            typeof(IMEGWindowLayout));
+
+        public static readonly DependencyProperty PrimaryButtonTextProperty =
+            DependencyProperty.Register(nameof(PrimaryButtonText), typeof(string),
+            typeof(IMEGWindowLayout), new PropertyMetadata("Apply"));
+
+        public static readonly DependencyProperty PrimaryCommandProperty =
+            DependencyProperty.Register(nameof(PrimaryCommand), typeof(ICommand),
+            typeof(IMEGWindowLayout));
+
+        public static readonly DependencyProperty CancelCommandProperty =
+            DependencyProperty.Register(nameof(CancelCommand), typeof(ICommand),
+            typeof(IMEGWindowLayout));
+
+        public string WindowTitle
+        {
+            get => (string)GetValue(WindowTitleProperty);
+            set => SetValue(WindowTitleProperty, value);
+        }
+
+        public object MainContent
+        {
+            get => GetValue(MainContentProperty);
+            set => SetValue(MainContentProperty, value);
+        }
+
+        public string PrimaryButtonText
+        {
+            get => (string)GetValue(PrimaryButtonTextProperty);
+            set => SetValue(PrimaryButtonTextProperty, value);
+        }
+
+        public ICommand PrimaryCommand
+        {
+            get => (ICommand)GetValue(PrimaryCommandProperty);
+            set => SetValue(PrimaryCommandProperty, value);
+        }
+
+        public ICommand CancelCommand
+        {
+            get => (ICommand)GetValue(CancelCommandProperty);
+            set => SetValue(CancelCommandProperty, value);
+        }
+
         public IMEGWindowLayout()
         {
             InitializeComponent();
